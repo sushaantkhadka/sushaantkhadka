48ffd2c285053c4a9b0444bf9ae8e80baca5b008
Sushant <sushantkhadka2060@gmail.com>
2024-05-27 17:56:44 +0545

    bug fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.getElementById('check');
     const background = document.getElementById('canvas')
+    const audio = document.getElementById('myAudio');
     const body = document.body;
 
     if (localStorage.getItem('darkMode') === 'enabled') {
@@ -8,7 +9,9 @@ document.addEventListener('DOMContentLoaded', function () {
         toggleButton.checked = true;
         background.style.display = 'flex';
         document.documentElement.style.setProperty('--global-color', '#00f73f');
-        document.getElementById('designation').innerHTML="Frontend Dev"
+        document.getElementById('designation').innerHTML="Frontend Dev";
+        audio.src= 'static/music/rock.mp3';
+        audio.play();
 
     }
 
@@ -18,37 +21,17 @@ document.addEventListener('DOMContentLoaded', function () {
             background.style.display = 'flex';
             document.documentElement.style.setProperty('--global-color', '#00f73f');
             document.getElementById('designation').innerHTML="Frontend Dev"
+            audio.src= 'static/music/rock.mp3';
+            audio.play();
             localStorage.setItem('darkMode', 'enabled');
         } else {
             body.classList.remove('dark-mode');
             background.style.display = 'none';
             document.documentElement.style.setProperty('--global-color', '#FF5733');
             document.getElementById('designation').innerHTML="UiUx Designer"
-            localStorage.setItem('darkMode', 'disabled');
-        }
-    });
-
-
-
-
-    const musicPlay = document.getElementById('checkboxInput');
-    const audio = document.getElementById('myAudio');
-
-    if (localStorage.getItem('musicPlaying') === 'false') {
-        musicPlay.checked = true;
-        audio.pause()
-    }else{
-        audio.play();
-    }
-
-
-    musicPlay.addEventListener('change', function() {
-        if (musicPlay.checked) {
-            audio.pause();
-            localStorage.setItem('musicPlaying', 'false');
-        } else {
+            audio.src= 'static/music/soft.mp3';
             audio.play();
-            localStorage.setItem('musicPlaying', 'true');
+            localStorage.setItem('darkMode', 'disabled');
         }
     });
 
