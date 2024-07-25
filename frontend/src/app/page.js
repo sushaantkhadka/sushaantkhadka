a7ba54d445a50630d675a526571df41e5039e6dd
Sushant <sushantkhadka2060@gmail.com>
2024-07-25 17:54:25 +0545

    dark and light mode

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,22 +3,24 @@ import React, { useState } from "react";
 import ProfileCard from "@/components/profileCard/ProfileCard";
 import ThemeToggle from "@/components/themeToggle/ThemeToggle";
 import MatrixEffect from "@/components/backgrounds/MatrixEffect";
+import LightMode from "@/components/portfolio/LightMode";
 // import darkModeAudio from "@/assets/audio/rock.mp3"
 // import lightModeAudio from "@/assets/audio/soft.mp3"
 
 
 export default function page() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-  // const darkModeMusic = new Audio(darkModeAudio);
-  // const lightModeMusic = new Audio(lightModeAudio);
+  // let darkModeMusic = new Audio(darkModeAudio);
+  // let lightModeMusic = new Audio(lightModeAudio);
+  
   const handleToggle = () => {
     setIsDarkMode(!isDarkMode);
     if (!isDarkMode) {
-    //   darkModeMusic.play();
-    //   // console.log('Checkbox is checked');
-    // } else {
-    //   lightModeMusic.play();
-    //   console.log('Checkbox is unchecked');
+      // darkModeMusic.play();
+      // console.log('Checkbox is checked');
+    } else {
+      // lightModeMusic.play();
+      console.log('Checkbox is unchecked');
     }
     document.body.classList.toggle("dark-mode", !isDarkMode);
   };
@@ -29,6 +31,7 @@ export default function page() {
       <div className="m-4 flex flex-col justify-center items-center">
         <ThemeToggle isDarkMode={isDarkMode} onToggle={handleToggle} />
         <ProfileCard theme={isDarkMode} />
+        <LightMode theme={isDarkMode} />
       </div>
     </>
   );
---
+++ frontend/src/components/portfolio/LightMode.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProfileCard from '../profileCard/ProfileCard'
+
+export default function LightMode({theme}) {
+  return (
+    <div>
+        
+    </div>
+  )
+}
