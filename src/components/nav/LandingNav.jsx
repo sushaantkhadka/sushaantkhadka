8b02e317028c49635781dc338342c02421d1c5f5
Sushant <sushantkhadka2060@gmail.com>
2024-07-30 13:25:45 +0545

    changes for deployment

--- src/components/nav/LandingNav.jsx
+++ src/components/nav/LandingNav.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 
 const LandingNav = ({ theme }) => {
   const navList = [
--- src/components/portfolio/DarkMode.jsx
+++ src/components/portfolio/DarkMode.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function DarkMode() {
   return (
     <div>
       <div className="flex flex-col justify-center items-center h-full">
-                <h1 className='text-white font-bold'>Github Contributions</h1>
-                <img className="" src="https://raw.githubusercontent.com/sushaantkhadka/sushaantkhadka/output/snake.svg"
-                    alt="" />
+        <h1 className="text-white font-bold">Github Contributions</h1>
+        <img src="https://raw.githubusercontent.com/sushaantkhadka/sushaantkhadka/output/snake.svg" />
 
-                <a href="https://github.com/sushaantkhadka" target="_blank"><button className="mt-2 rounded-full px-4 resume-dark border-green-500 border-2 text-sm text-green-500">VISIT MY
-                        GITHUB</button>
-                </a>
-            </div>
+        <a href="https://github.com/sushaantkhadka" target="_blank">
+          <button className="mt-2 rounded-full px-4 resume-dark border-green-500 border-2 text-sm text-green-500">
+            VISIT MY GITHUB
+          </button>
+        </a>
+      </div>
     </div>
-  )
+  );
 }
