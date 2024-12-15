232070c21c778b55de76ba552707c3e83634d233
Sushant <sushantkhadka2060@gmail.com>
2024-12-15 15:24:37 +0545

    change

--- next.config.mjs
+++ next.config.mjs
@@ -5,24 +5,25 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-      config.module.rules.push({
-        test: /\.(mp3|wav|ogg)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'static/media/',
-            publicPath: '/_next/static/media/',
-          },
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(mp3|wav|ogg)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "static/media/",
+          publicPath: "/_next/static/media/",
         },
-      });
-  
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+      },
+    });
 
+    return config;
+  },
+};
 
+export default nextConfig;
