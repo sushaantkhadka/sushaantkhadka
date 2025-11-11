749f32603e9374780e7223d0c8017efa3320e05b
Sushant <Sushantkhadka2060@gmail.com>
2025-11-11 17:00:34 +0545

    Remove peer dependencies from package-lock.json and add new image file Photos.png

--- app/components/Photos.tsx
+++ app/components/Photos.tsx
@@ -6,17 +6,22 @@ import { Heart, LayoutDashboard, Map, Smile } from "lucide-react";
 export default function () {
   return (
     <div className="relative">
-      <div className="absolute px-3 py-3 flex gap-1 rounded-full glass-card h-fit">
-        <LayoutDashboard />
+      <div className="absolute px-9 py-8 w-full h-full flex flex-col  justify-between items-end" >
+        <Image
+          src="/Photos.png"
+          alt="Photos icon" width={1080} height={1080} className="w-14 h-14 rounded-lg" />
+        <div className=" px-3 py-3 gap-3 flex rounded-full glass-card h-fit fill-current text-[#49454F]">
+        <span className="py-1.5 px-4 bg-white rounded-full"> <LayoutDashboard /></span>
         <Map />
         <Heart />
         <Smile />
       </div>
+      </div>
       <Image
         src="/sky.jpg"
         alt="Photos"
-        width={800}
-        height={600}
+        width={1600}
+        height={900}
         className={`w-[524px] h-[350px] rounded-4xl`}
       />
     </div>
